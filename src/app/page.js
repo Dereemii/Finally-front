2dbf07646e3f5304c74e40ec53bb75572cddca80
Leslie Herrera - Dereemii <leshermira@gmail.com>
2024-08-24 17:07:22 -0400

    add buttons

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
+import { Button } from '../components/Button/Button'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Finally</h1>
+      <Button/>
     </main>
   );
 }
