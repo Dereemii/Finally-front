94719db45184f2cf212555624faef8a271a10c8e
Leslie Herrera - Dereemii <leshermira@gmail.com>
2024-08-24 17:37:53 -0400

    format files

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Finally</h1>
-      <BaseButton text="holaa" bgColor='bg-success'/>
+      <BaseButton text="holaa" bgColor="bg-success" />
     </main>
   );
 }
