b5e35a539aef46769f0bbe71cb2b44e0dfe821c1
Leslie Herrera - Dereemii <leshermira@gmail.com>
2024-08-24 17:11:22 -0400

    chore: change button name

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import { Button } from '../components/Button/Button'
+import { BaseButton } from '../components/BaseButton/BaseButton'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Finally</h1>
-      <Button/>
+      <BaseButton/>
     </main>
   );
 }
--- src/components/BaseButton/BaseButton.jsx
+++ src/components/BaseButton/BaseButton.jsx
@@ -1,4 +1,4 @@
-export const Button = () => {
+export const BaseButton = () => {
   return (
     <>
       <button
