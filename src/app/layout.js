23501c4400776188b274cbc915dccaa0e0ab1212
Cecy Geraldo <cgeraldoq@gmail.com>
2024-08-24 15:41:34 -0400

    chore: add and config prettier for nextjs and tailwind

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,11 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "FinAlly",
-  description: "Financial app",
+  title: 'FinAlly',
+  description: 'Financial app',
 };
 
 export default function RootLayout({ children }) {
