bd8f4cfbbc8ac86a4bf6948360e0cd94168ed914
Cecy Geraldo <cgeraldoq@gmail.com>
2024-08-25 02:33:40 -0400

    feat: create FloatingButton component with clipboardIcon svg

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import { BaseButton } from '../components/BaseButton/BaseButton';
+import { FloatingButton } from '../components/FloatingButton';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Finally</h1>
       <BaseButton text="holaa" bgColor="bg-success" />
+      <FloatingButton />
     </main>
   );
 }
