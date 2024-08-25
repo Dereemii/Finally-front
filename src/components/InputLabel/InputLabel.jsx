dbf275b29d5c101f1f5785013fa0d455df9d120d
Leslie Herrera - Dereemii <leshermira@gmail.com>
2024-08-25 06:06:38 -0400

    feat: add page goals

--- src/components/InputLabel/InputLabel.jsx
+++ src/components/InputLabel/InputLabel.jsx
@@ -4,13 +4,18 @@
  * @component
  * @param {Object} props - The component props.
  * @param {string} props.label - The label for the input.
+ * @param {string} props.placeholder - The placeholder for the input.
  * @returns {JSX.Element} The rendered InputLabel component.
  */
-export const InputLabel = ({ label }) => {
+export const InputLabel = ({ label, placeholder }) => {
   return (
     <div className="flex w-full flex-col py-3.5">
       {label && <label className="mb-2">{label}</label>}
-      <input type="text" className="rounded-md border border-gray-300 p-2 text-base" />
+      <input
+        type="text"
+        className="rounded-md border border-gray-300 p-2 text-base"
+        placeholder={placeholder}
+      />
     </div>
   );
 };
